3e6129b18b247acea855211d430b0c19d6edd80e
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-07-31 22:15:08 -0700

    Created getWeather and populateWeather functions that fetch weather data based on location arg and inserts data onto DOM

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+const submitBtn = document.querySelector('#submit-btn');
+const input = document.querySelector('#location-input');
+
 async function getWeather(location) {
-    location = location.toLowerCase().replace(' ', '_');
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=981d41ec437c477d9f042719232907&q=${location}`);
-    const data = await response.json();
-    console.log(data);
+    if (location) {
+        location = location.toLowerCase().replace(' ', '_');
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=981d41ec437c477d9f042719232907&q=${location}`);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } else return null;
+}
+
+async function populateWeather(input) {
+    try {
+        const locOutput = document.querySelector('#location');
+        const tempfOutput = document.querySelector('#tempf');
+        const tempcOutput = document.querySelector('#tempc');
+        const weatherData = await getWeather(input.value);
+
+        locOutput.textContent = weatherData.location.name;
+        tempfOutput.textContent = `${weatherData.current.temp_f} °F`;
+        tempcOutput.textContent = `${weatherData.current.temp_c} °C`;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const submitBtn = document.querySelector('#submit');
-    const input = document.querySelector('#location');
-    submitBtn.addEventListener('click', () => {
-        getWeather(input.value);
-    });
-})
+    submitBtn.addEventListener('click', () => populateWeather(input));
+});
+
